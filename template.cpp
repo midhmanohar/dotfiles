dc01b7791568f7e4d17fe35c5e17d90f0a3b20ca
midhun manohar <midhmanohar@gmail.com>
2019-09-09 17:42:37 +0530

    new files

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define s(x) scanf("%d", &x)
+#define vi vector<int>
+#define vll vector<long long>
+#define vii vector<vi>
+#define sll(x) scanf("%lld", &x)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+typedef long long ll;
+
+
+/////////////////////////////////////////////////////////////////////
+
+int main(){
+
+    
+    
+    return 0;
+
+}
